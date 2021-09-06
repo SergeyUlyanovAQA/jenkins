aac5e17affc6681e7b65d02397b6c6fcc361fd8a
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-09-06 21:53:28 +0400

    added properties

---
+++ src/test/java/test/properties/SystemPropertiesTests.java
@@ -0,0 +1,58 @@
+package test.properties;
+
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("properties")
+public class SystemPropertiesTests {
+    @Test
+    void test1() {
+        System.out.println("[test1] Browser: " + System.getProperty("browser"));
+    }
+
+    @Test
+    void readNullFromPropertyTest() {
+        String value = System.getProperty("our_property");
+        System.out.println(value);
+        // null
+    }
+
+    @Test
+    void readDefaultFromPropertyTest() {
+        String value = System.getProperty("our_property", "default_value");
+        System.out.println(value);
+        // default_value
+    }
+
+    @Test
+    void readWithTerminalFromPropertyTest() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+
+
+        //  gradle clean properties_tests
+        //  gradle clean properties_tests -Dbrowser=safari
+        //  gradle clean properties_tests -Dbrowser=safari -Dversion=89
+    }
+
+    @Test
+    void setPropertyTest() {
+
+        System.out.println(System.getProperty("browser"));
+        System.setProperty("browser", "firefox");
+
+        System.out.println(System.getProperty("browser"));
+
+
+        //  gradle clean properties_tests
+        //  gradle clean properties_tests -Dbrowser=safari
+        //  gradle clean properties_tests -Dbrowser=safari -Dversion=89
+    }
+
+}
